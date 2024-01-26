300275f367db1beb76aa8b6d85f119a5eea04e95
Bram del Canho <bramdelcanho@gmail.com>
2024-01-26 20:21:09 +0100

    Updated readme

---
+++ README.md
@@ -0,0 +1,73 @@
+# Express List Endpoints
+
+[![NPM](https://nodei.co/npm/express-list-endpoints-2.png)](https://nodei.co/npm/express-list-endpoints-2/)
+
+Express endpoint parser to retrieve a list of the passed router with the set verbs.
+
+## Example of use
+
+Warning: In contrast to the original package, this package does not merge the endpoints which are initialized with the same path.
+
+```javascript
+import listEndpoints from 'express-list-endpoints-2'
+
+const app = require('express')();
+
+app.route('/')
+    .all(function namedMiddleware(req, res) {
+        // Handle request
+    })
+    .get((req, res) => {
+        // Handle request
+    })
+    .post((req, res) => {
+        // Handle request
+    });
+
+app.route('/')
+    .all(function namedMiddleware2(req, res) {
+        // Handle request
+    })
+    .post((req, res) => {
+        // Handle request
+    });
+
+app.route('/about')
+    .get((req, res) => {
+        // Handle request
+    })
+
+console.log(listEndpoints(app));
+
+/* It omits the 'all' verbs.
+[
+    {
+        path: '/',
+        methods: ['GET', 'POST'],
+        middlewares: [[Function namedMiddleware], [Function anonymous], [Function anonymous]],
+    },
+    {
+        path: '/',
+        methods: ['POST'],
+        middlewares: [[Function namedMiddleware2], [Function anonymous]],
+    },
+    {
+        path: '/about',
+        methods: ['GET'],
+        middlewares: [[Function anonymous]],
+    },
+]
+*/
+```
+
+## Arguments
+
+### `app` - Express `app` or `router` instance
+
+Your router instance (`router`) or your app instance (`app`).
+
+_**Note:** Pay attention that before call this script the router or app must have the endpoints registered due to detect them._
+
+## License
+
+Express List Endpoints is [MIT licensed](./LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,148 @@
+import express from 'express'
+
+const regExpToParseExpressPathRegExp = /^\/\^\\\/(?:(:?[\w\\.-]*(?:\\\/:?[\w\\.-]*)*)|(\(\?:\(\[\^\\\/]\+\?\)\)))\\\/.*/
+const regExpToReplaceExpressPathRegExpParams = /\(\?:\(\[\^\\\/]\+\?\)\)/
+const regexpExpressParamRegexp = /\(\?:\(\[\^\\\/]\+\?\)\)/g
+
+const EXPRESS_ROOT_PATH_REGEXP_VALUE = '/^\\/?(?=\\/|$)/i'
+const STACK_ITEM_VALID_NAMES: string[] = [
+    'router',
+    'bound dispatch',
+    'mounted_app'
+]
+
+interface Endpoint {
+    path: string
+    methods: string[]
+    middlewares: express.RequestHandler[]
+}
+
+/**
+ * Returns all the verbs detected for the passed route
+ */
+const getRouteMethods = function (route: any): string[] {
+    let methods = Object.keys(route.methods)
+
+    methods = methods.filter((method) => method !== '_all')
+    methods = methods.map((method) => method.toUpperCase())
+
+    return methods
+}
+
+/**
+ * Returns the names (or anonymous) of all the middlewares attached to the
+ * passed route
+ */
+const getRouteMiddlewares = function (route: any): express.RequestHandler[] {
+    return route.stack.map((item: any) => {
+        return item.handle
+    })
+}
+
+/**
+ * Returns true if found regexp related with express params
+ */
+const hasParams = function (expressPathRegExp: string): boolean {
+    return regexpExpressParamRegexp.test(expressPathRegExp)
+}
+
+const parseExpressRoute = function (route: any, basePath: string): Endpoint[] {
+    const paths: string[] = []
+
+    if (Array.isArray(route.path)) {
+        paths.push(...route.path)
+    } else {
+        paths.push(route.path)
+    }
+
+    const endpoints = paths.map((path) => {
+        const completePath = basePath && path === '/'
+            ? basePath
+            : `${basePath}${path}`
+
+        const endpoint = {
+            path: completePath,
+            methods: getRouteMethods(route),
+            middlewares: getRouteMiddlewares(route)
+        }
+
+        return endpoint
+    })
+
+    return endpoints
+}
+
+const parseExpressPath = function (expressPathRegExp: RegExp, params: any[]): string {
+    let expressPathRegExpExec = regExpToParseExpressPathRegExp.exec(expressPathRegExp.toString())
+    let parsedRegExp = expressPathRegExp.toString()
+    let paramIndex = 0
+
+    while (hasParams(parsedRegExp)) {
+        const paramName = params[paramIndex].name
+        const paramId = `:${paramName}`
+
+        parsedRegExp = parsedRegExp
+            .replace(regExpToReplaceExpressPathRegExpParams, paramId)
+
+        paramIndex++
+    }
+
+    if (parsedRegExp !== expressPathRegExp.toString()) {
+        expressPathRegExpExec = regExpToParseExpressPathRegExp.exec(parsedRegExp)
+    }
+
+    const parsedPath = expressPathRegExpExec![1].replace(/\\\//g, '/')
+
+    return parsedPath
+}
+
+const parseEndpoints = function (app: express.Express, basePath?: string, endpoints?: Endpoint[]): Endpoint[] {
+    const stack = app.stack || (app._router && app._router.stack)
+
+    endpoints = endpoints || []
+    basePath = basePath || ''
+
+    if (stack)
+        endpoints = parseStack(stack, basePath, endpoints)
+
+    return endpoints
+}
+
+const parseStack = function (stack: any[], basePath: string, endpoints: Endpoint[]): any[] {
+    stack.forEach((stackItem) => {
+        if (stackItem.route) {
+            const newEndpoints = parseExpressRoute(stackItem.route, basePath)
+
+            endpoints.push(...newEndpoints)
+        } else if (STACK_ITEM_VALID_NAMES.includes(stackItem.name)) {
+            const isExpressPathRegexp = regExpToParseExpressPathRegExp.test(stackItem.regexp)
+
+            let newBasePath = basePath
+
+            if (isExpressPathRegexp) {
+                const parsedPath = parseExpressPath(stackItem.regexp, stackItem.keys)
+
+                newBasePath += `/${parsedPath}`
+            } else if (!stackItem.path && stackItem.regexp && stackItem.regexp.toString() !== EXPRESS_ROOT_PATH_REGEXP_VALUE) {
+                const regExpPath = ` RegExp(${stackItem.regexp}) `
+
+                newBasePath += `/${regExpPath}`
+            }
+
+            endpoints = parseEndpoints(stackItem.handle, newBasePath, endpoints)
+        }
+    })
+
+    return endpoints
+}
+
+/**
+ * Returns an array of strings with all the detected endpoints
+ */
+const getEndpoints = function (app: express.Express): Endpoint[] {
+    const endpoints = parseEndpoints(app)
+
+    return endpoints
+}
+
+export default getEndpoints
